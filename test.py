a043eb6b999d6213a43c6c7971e49ae59f0faa69
Jihui Yuan <2108248826@qq.com>
2026-05-09 13:23:37 +0800

    add test2

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import torch
-import numpy
-import clip
-from PIL import image
+import numpy as np
+# import clip
+# from PIL import Image
 
+# print(Image.__version__)
+# # 将 PIL 图像转换为矩阵，方便进行归一化或光谱分析
+# img_array = np.array(Image.open("126035_124_3.png"))
+# print (torch.__version__)
 
-print
+print(torch.__version__)
+print(np.__version__)
